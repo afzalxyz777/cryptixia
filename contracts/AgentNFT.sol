d1c86a1734d927bcfc18a308c805435ee3c31b0c
Afzal Amanullah <afzalamanullah@Afzals-MacBook-Air.local>
2025-08-24 12:58:10 +0530

    fix: formatted contracts to pass CI

--- contracts/AgentNFT.sol
+++ contracts/AgentNFT.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import{ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721URIStorage} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -12,15 +12,15 @@ contract AgentNFT is ERC721URIStorage, Ownable {
     event AgentMinted(uint256 indexed tokenId, address indexed owner, string tokenUri);
 
     // Constructor sets NFT name, symbol and contract owner
-    constructor() ERC721("AgentNFT", "AGNT") Ownable(msg.sender){
+    constructor() ERC721("AgentNFT", "AGNT") Ownable(msg.sender) {
         // Ownable automatically sets msg.sender as owner
     }
 
     // Mint a new Agent NFT to address `to` with metadata URI
     function mint(address to, string memory tokenUri) external onlyOwner {
-        _safeMint(to, nextTokenId);      // Mint token
+        _safeMint(to, nextTokenId); // Mint token
         _setTokenURI(nextTokenId, tokenUri); // Set token metadata
         emit AgentMinted(nextTokenId, to, tokenUri); // Emit event
-        nextTokenId++;                   // Increment ID for next mint
+        nextTokenId++; // Increment ID for next mint
     }
 }
